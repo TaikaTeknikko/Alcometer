d091ddce5973ffc527d003e628eb7a7efab06675
Teemu <teemu.salonen96@hotmail.com>
2020-01-23 14:22:48 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  weight: number;
+  gender: string;
+  genders = [];
+  time: number;
+  bottles: number;
+  promilles: number;
+
 
   constructor() {}
 
+  ngOnInit() {
+    this.genders.push("Male");
+    this.genders.push("Female");
+
+    this.gender="Male";
+    this.weight=80;
+    this.time=6;
+    this.bottles=12;
+
+  }
+  calculate() {
+    const litres = this.bottles * 0.33;
+    let grams = litres * 8 * 4.5;
+
+    const burning = this.weight / 10;
+    grams = grams - (burning * this.time);
+
+    if (this.gender === "Male") {
+      this.promilles = grams / (this.weight * 0.7);
+    }else {
+      this.promilles = grams / (this.weight * 0.7);
+    }
+
+  }
 }
