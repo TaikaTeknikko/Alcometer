39c67d6ecd4c2fadf3ebb0950d445429756dc5cf
Teemu <teemu.salonen96@hotmail.com>
2020-02-03 08:43:27 +0200

    final version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,35 +9,49 @@ export class HomePage implements OnInit{
   weight: number;
   gender: string;
   genders = [];
-  time: number;
-  bottles: number;
+  times = [];
+  time: string;
+  bottle: string;
+  bottles = [];
   promilles: number;
 
-
+  
   constructor() {}
 
   ngOnInit() {
     this.genders.push("Male");
     this.genders.push("Female");
 
-    this.gender="Male";
-    this.weight=80;
-    this.time=6;
-    this.bottles=12;
-
-  }
-  calculate() {
-    const litres = this.bottles * 0.33;
-    let grams = litres * 8 * 4.5;
-
-    const burning = this.weight / 10;
-    grams = grams - (burning * this.time);
-
-    if (this.gender === "Male") {
-      this.promilles = grams / (this.weight * 0.7);
-    }else {
-      this.promilles = grams / (this.weight * 0.7);
+    for(let i=0; i<=30; i++){
+      this.bottles.push(i);
     }
 
+    for(let i=0; i<=24; i++){
+      this.times.push(i);
+    }
+
+      this.gender="Male";
+      this.weight=80;
+      this.time="6";
+      this.bottle="12"; 
   }
-}
+    
+
+    calculate() {
+      const litres = parseInt(this.bottle) * 0.33;
+      let grams = litres * 8 * 4.5;
+  
+      const burning = this.weight / 10;
+      grams = grams - (burning * parseInt(this.time));
+  
+      if (this.gender === "Male") {
+        this.promilles = grams / (this.weight * 0.7);
+      }else {
+        this.promilles = grams / (this.weight * 0.6);
+      }
+      if (this.promilles < 0) {
+        this.promilles = 0;
+      }
+  
+    }
+  }
